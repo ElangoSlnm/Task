fde84e142eceffb2425ac81a4e875c99bf55440d
ElangoSlnm <42702028+ElangoSlnm@users.noreply.github.com>
2020-07-13 11:50:29 +0530

    Update ubuntu_container.sh

--- docker/ubuntu_container.sh
+++ docker/ubuntu_container.sh
@@ -1,1 +1,9 @@
+# create ubuntu container
 sudo docker run -it -P --privileged --name spark-node ubuntu /bin/bash
+
+# start existing container
+sudo docker start spark-node
+
+# execute docker container
+sudo docker exec -it --privileged spark-node /bin/bash
+
